36496a97a888b2e7c2d66c6ef15587b493e34800
Anuj <anuj.vgithub25@gmail.com>
2025-03-29 17:00:30 +0530

    changes for client code

---
+++ client/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+
+const App: React.FC = () => {
+  return <>
+        <h1>Hello World</h1>
+    </>;
+};
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
